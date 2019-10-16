dd515f4d0b42e0b5c4efff5e7ba46dc94d95d2b5
jojo0severo <joaovbsevero@gmail.com>
2019-10-16 17:53:22 -0300

    Created empty browser controller

---
+++ controller/browser_controller.py
@@ -0,0 +1,14 @@
+
+
+class BrowserController:
+    def __init__(self):
+        pass
+
+    def open_gmail(self):
+        pass
+
+    def open_facebook(self):
+        pass
+
+    def open_google(self):
+        pass
